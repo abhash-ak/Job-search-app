bc65854fbbfee78ca291662a3811fdcc52dc5d43
abhash-ak <abhash_2101mc01@iitp.ac.in>
2024-02-16 20:57:41 +0530

    changed

--- app/search/[id].js
+++ app/search/[id].js
@@ -26,7 +26,7 @@ const JobSearch = () => {
                 method: "GET",
                 url: `https://jsearch.p.rapidapi.com/search`,
                 headers: {
-                    "X-RapidAPI-Key": 'daac71651dmshb73bfaee0dc5c2bp1800a7jsn13513f792426',
+                    "X-RapidAPI-Key": '',
                     "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
                 },
                 params: {
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -11,7 +11,7 @@ const useFetch = (endpoint, query) => {
     method: 'GET',
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      'X-RapidAPI-Key': 'daac71651dmshb73bfaee0dc5c2bp1800a7jsn13513f792426',
+      'X-RapidAPI-Key': '',
       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
     },
     params: { ...query },
